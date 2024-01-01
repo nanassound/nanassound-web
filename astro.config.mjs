6f42eda8dffa28754297c76c927362201d86a206
Asep Bagja Priandana <bepitulaz@gmail.com>
2024-01-01 18:14:55 +0200

    Remove bulma config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,6 @@ import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      noExternal: ["bulma"]
-    }
-  },
   markdown: {
     drafts: true
   },
