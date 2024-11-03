2ea3cdcf3c5f328d3b5821f7e2cb8a694b4f3f0b
Asep Bagja Priandana <bepitulaz@gmail.com>
2024-11-03 19:45:11 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/functions";
 
 import tailwind from "@astrojs/tailwind";
 
