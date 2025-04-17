8ab46a7ea591b5589bb46506757d28f51d8c7b1a
Asep Bagja Priandana <bepitulaz@gmail.com>
2025-04-17 11:44:15 +0300

    Remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
-import netlify from "@astrojs/netlify";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -18,7 +15,6 @@ export default defineConfig({
     drafts: true
   }), tailwind()],
   output: "server",
-  adapter: netlify(),
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
